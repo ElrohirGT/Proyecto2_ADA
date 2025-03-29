4d274620297e87de36c7538dfae43e69847dcdcc
ElrohirGT <elrohirgt@gmail.com>
2025-03-28 19:39:33 -0600

    feat: Added some compilation code

--- PD/main.go
+++ PD/main.go
@@ -1,1 +1,28 @@
 package main
+
+type ProblemInfo struct {
+	VolumenInicial uint8
+	MaximaCapacidadBombeo uint8
+	CosteDecisionesPorUnidad uint8
+	CosteAlmacenamientoPorUnidad float64
+	CosteDemandaNoSatisfechaPorUnidad uint8
+}
+
+
+func GetDefaultProblemContext() ProblemInfo {
+	return ProblemInfo{
+	VolumenInicial : 1,
+	MaximaCapacidadBombeo : 2,
+	CosteDecisionesPorUnidad : 1,
+	CosteAlmacenamientoPorUnidad : 0.1,
+	CosteDemandaNoSatisfechaPorUnidad : 2,
+}
+}
+
+func optimize(info *ProblemInfo, week *[]uint8) {
+
+}
+
+func main() {
+
+}
